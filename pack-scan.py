ac77d10e0adf6d0f4ec45b8a32828b52eacfec6c
denNorske <dennorske@users.noreply.github.com>
2021-03-19 21:18:02 +0100

    FIX: Spacing between all querythread packets.

--- pack-scan.py
+++ pack-scan.py
@@ -6,10 +6,10 @@
 #    iptables -t nat -A PREROUTING -p udp --dport 7777 -s 127.0.0.1 -m string --algo bm --string 'SAMP' -j REDIRECT --to-port 7777
 #    iptables -t nat -A PREROUTING -p udp --dport 7777 -m string --algo bm --string 'SAMP' -j REDIRECT --to-port 7778
 #    iptables -I INPUT -p udp --dport 7778 -m string --algo bm --string 'SAMP' -m hashlimit ! --hashlimit-upto 10/sec --hashlimit-burst 15/sec --hashlimit-mode srcip --hashlimit-name query -j DROP
-#    
+#
 #
 #    make sure ports above match ports below
-#    
+#
 #    Basicly all query packets get directed to this script, and sync packets are left untouched
 #
 
@@ -21,12 +21,12 @@ import threading
 import time
 import binascii
 ####################### MUST BE CONFIGURED ##########################
-SERVER_PORT = 7777 #Your CURRENT samp server port. 
-PROXY_PORT = 7778 #Code hijacks this. Code will then filter, and send it to SERVER_PORT
-SAMP_SERVER_ADDRESS = "Your ip" #Public ip - set this to the ip you using in bind in your server.cfg
+SERVER_PORT = 7777 #Assuming your samp server runs on this port
+PROXY_PORT = 7778 #Assuming no other servers are running on this one, as it will be taken by the code.
+SAMP_SERVER_ADDRESS = "INSERT PUBLIC IP" #Public ip set this to the ip you using in bind in your server.cfg
 #####################################################################
 
-SAMP_SERVER_LOCALHOST = "127.0.0.1" #Edit this if you run this on a different server than the samp server. Otherwise, let it be.
+SAMP_SERVER_LOCALHOST = "127.0.0.1" #Edit this if you run this on a different server than the samp server
 SAMP_SERVER_ADDRESS_BYTES = socket.inet_aton(SAMP_SERVER_ADDRESS)
 
 info = " "
@@ -41,7 +41,7 @@ iplog = []
 
 
 class UDPServer:
-  def __init__(self, bind_address, target_address, internal_host = "127.0.0.1", timeout = 0.3): #ulimit?
+  def __init__(self, bind_address, target_address, internal_host = "127.0.0.1", timeout = 0.5): #ulimit?
     self.target_address = target_address
     self.timeout = timeout
 
@@ -55,40 +55,40 @@ class UDPServer:
     global detail
     global isonline
 
-    socket.setdefaulttimeout(1)
+    socket.setdefaulttimeout(4)
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
-      
       if self.ping():
-
+        isonline = True
+        time.sleep(1)
         packet = self.assemblePacket("i")
+        print(packet)
         self.sock.sendto(packet, (SAMP_SERVER_ADDRESS, SERVER_PORT))
         info = self.sock.recv(1024)[11:]
 
+        time.sleep(1)
         packet = self.assemblePacket("r")
+        print(packet)
         self.sock.sendto(packet, (SAMP_SERVER_ADDRESS, SERVER_PORT))
         rules = self.sock.recv(1024)[11:]
 
+        time.sleep(1)
         packet = self.assemblePacket("d")
+        print(packet)
         self.sock.sendto(packet, (SAMP_SERVER_ADDRESS, SERVER_PORT))
         detail = self.sock.recv(1024)[11:]
-        
-        #To avoid timeouts on the socket, we postpone the clientlist from the other
-        #requests. Seems like it refuses to reply all 4 packets at once.
-        time.sleep(2)
+
+        time.sleep(1)
         packet = self.assemblePacket("c")
+        print(packet)
         self.sock.sendto(packet, (SAMP_SERVER_ADDRESS, SERVER_PORT))
         clients = self.sock.recv(1024)[11:]
 
-        isonline = True
-
-        time.sleep(3)
       else:
         isonline = False
-        print("Server unable to be reached...")
+        print("Server unable to be reached. Trying again...")
 
   def ping(self):
-
     pack = self.assemblePacket("p0101")
     self.sock.sendto(pack, (SAMP_SERVER_ADDRESS, SERVER_PORT))
     try:
@@ -99,7 +99,7 @@ class UDPServer:
         return False
     except socket.timeout:
       return False
-      
+
   def assemblePacket(self, type):
     PUBLIC_PORT_BYTES = SERVER_PORT.to_bytes(2, byteorder='little')
     packet = b'SAMP'
@@ -111,11 +111,11 @@ class UDPServer:
 
 
   def start(self):
+    self.server.socket.settimeout(self.timeout)
     q = threading.Thread(target=self.querythread)
     q.daemon = True
     q.start()
     self.server.serve_forever()
-    self.server.socket.settimeout(self.timeout)
 
 
   def stop(self):
@@ -126,13 +126,13 @@ class UDPServer:
     client_address = handler.client_address
     if(isonline == False):
       return
-    if payload[4:8] != SAMP_SERVER_ADDRESS_BYTES: #Payload with IP bytes are not matching your public IP
-      return False 
-    
-    if payload[10] not in b'pirdc': #opcodes defined here: https://wiki.sa-mp.com/wiki/Query/Request#Opcodes
-      return False 
+    if payload[4:8] != SAMP_SERVER_ADDRESS_BYTES: # Payload with IP bytes are not matching your public IP
+      return False
 
-    if payload[10] in b'p': #Ping packets are just sent back to the client 
+    if payload[10] not in b'pirdc': #opcodes defined here: https://wiki.sa-mp.com/wiki/Query/Request#Opcodes
+      return False
+
+    if payload[10] in b'p': #Ping packets are just sent back to the client
       client_address = handler.client_address
       self.server.socket.sendto(payload, client_address)
       return True
@@ -143,30 +143,27 @@ class UDPServer:
       argument = payload+info
       self.server.socket.sendto(argument, client_address)
       return True
-      
 
-    elif payload[10] in b'r': 
+
+    elif payload[10] in b'r':
       client_address = handler.client_address
       argument = payload+rules
       self.server.socket.sendto(argument, client_address)
-      #print(binascii.hexlify(payload+rules))
       return True
-      
+
 
     elif payload[10] in b'd':
       client_address = handler.client_address
       argument = payload+detail
       self.server.socket.sendto(argument, client_address)
       return True
-      
+
 
     elif payload[10] in b'c':
       client_address = handler.client_address
       argument = payload+clients
       self.server.socket.sendto(argument, client_address)
-      #thebytes=payload+clients
-      #print(len(payload+clients))
-      return True 
+      return True
     return
 
 def create_handler(func):
@@ -180,8 +177,8 @@ def create_handler(func):
   return Handler
 
 if __name__ == '__main__':
-  print("Listening on port", PROXY_PORT, "for server",SAMP_SERVER_ADDRESS," that runs on port",SERVER_PORT)
-  bind_address = (SAMP_SERVER_ADDRESS, PROXY_PORT) 
+  print("Listening on port", PROXY_PORT, "for server",SAMP_SERVER_ADDRESS,"on port",SERVER_PORT)
+  bind_address = (SAMP_SERVER_ADDRESS, PROXY_PORT)
   target_address = ("127.0.0.1", SERVER_PORT)
   proxy = UDPServer(bind_address, target_address)
   proxy.start()
